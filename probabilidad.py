ece383ff14de714c38fcf2bd57cce64a9d9173ad
jumanch <44240283+jumanch@users.noreply.github.com>
2019-05-03 19:52:50 +0200

    Add files via upload

---
+++ probabilidad.py
@@ -0,0 +1,32 @@
+import random
+
+pregunta = str(input("Escribe de que quieres comprobar: "))
+probabilidad= int(random.randint(0, 100))
+probabilidad= probabilidad-2
+separador = "="
+
+def grafico():
+    print("0%                      25%                      50%                      75%                      100%")
+    print("|~~~~~~~~~~~~~~~~~~~~~~~~|~~~~~~~~~~~~~~~~~~~~~~~~|~~~~~~~~~~~~~~~~~~~~~~~~|~~~~~~~~~~~~~~~~~~~~~~~~|")
+    print(f"|{separador*probabilidad}|")
+    print("|~~~~~~~~~~~~~~~~~~~~~~~~|~~~~~~~~~~~~~~~~~~~~~~~~|~~~~~~~~~~~~~~~~~~~~~~~~|~~~~~~~~~~~~~~~~~~~~~~~~|")
+
+if probabilidad >= 90:
+    grafico()
+    print("-¡Enhorabuena!")
+elif probabilidad in range(60,90):
+    grafico()
+    print("- Ni tan mal.")
+elif probabilidad in range(40,60):
+    grafico()
+    print("- UUUUUUuufff.")
+elif probabilidad in range(20,40):
+    grafico()
+    print("-Lo siento.")  
+else:
+    grafico()
+    print("- F.")
+
+print(f"Hay un {probabilidad}% de probabilidades de {pregunta}.")
+
+
